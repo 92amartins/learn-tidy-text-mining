7a11d35bf5a1a9382cdea211268e00e341e63be5
Andrei Martins <andrei.martins@esx.com.br>
2017-06-23 17:09:27 -0300

    Analyzing bigram

--- chapter4.R
+++ chapter4.R
@@ -3,17 +3,17 @@ library(tidyr)
 library(tidytext)
 library(janeaustenr)
 
-# Find bigrams
+# Find bigram
 austen_bigrams <- austen_books() %>%
   unnest_tokens(bigram, text, token = "ngrams", n = 2)
 
 austen_bigrams
 
-# Most common bigrams
+# Most common bigram
 austen_bigrams %>%
   count(bigram, sort = TRUE)
 
-# Split bigrams and filter stopwords
+# Split bigram and filter stopwords
 bigrams_separated <- austen_bigrams %>%
   separate(bigram, c("word1", "word2"), sep = " ")
 
@@ -21,7 +21,7 @@ bigrams_filtered <- bigrams_separated %>%
   filter(!word1 %in% stop_words$word) %>%
   filter(!word2 %in% stop_words$word)
   
-# new bigram counts:
+# New bigram counts:
 bigram_counts <- bigrams_filtered %>%
   count(word1, word2, sort = TRUE)
 
@@ -30,4 +30,37 @@ bigrams_united <- bigrams_filtered %>%
 
 bigrams_united
 
-#todo trigrama...
+# Trigram
+austen_books() %>%
+  unnest_tokens(trigram, text, token = "ngrams", n = 3) %>%
+  separate(trigram, c("word1", "word2", "word3"), sep = " ") %>%
+  filter(!word1 %in% stop_words$word,
+         !word2 %in% stop_words$word,
+         !word3 %in% stop_words$word) %>%
+  count(word1, word2, word3, sort = TRUE)
+
+# Analyzing bigrams
+bigrams_filtered %>%
+  filter(word2 == "street") %>%
+  count(book, word1, sort = TRUE)
+
+bigram_tf_idf <- bigrams_united %>%
+  count(book, bigram) %>%
+  bind_tf_idf(bigram, book, n) %>%
+  arrange(desc(tf_idf))
+
+bigram_tf_idf
+
+# Plot most relevant bigram by book
+bigram_tf_idf %>%
+  arrange(desc(tf_idf)) %>%
+  group_by(book) %>%
+  top_n(15, tf_idf) %>%
+  ungroup() %>%
+  mutate(bigram = reorder(bigram, tf_idf)) %>%
+  ggplot(aes(bigram, tf_idf, fill = book)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~book, ncol = 2, scales = "free") +
+  coord_flip()
+
